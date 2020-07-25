d52e000d623ebf492c52e1883fc9bc0b7928c40d
eric <eric@beelink.home>
2020-07-25 21:07:13 +0000

    upgrade

---
+++ usr/local/bin/upgrade.sh
@@ -0,0 +1,4 @@
+sudo apt update        # Fetches the list of available updates
+sudo apt full-upgrade  # Installs updates; may also remove some packages, if needed
+sudo apt autoremove    # Removes any old packages that are no longer needed
+reboot
